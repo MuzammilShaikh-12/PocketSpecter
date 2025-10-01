200b0d8c051d7894d829d32c80766f10591439d2
MuzammilShaikh-12 <mdmuzammilshaikh12@gmail.com>
2025-10-01 23:41:03 +0530

    Feat(frontend): -Implement Navbar component with dynamic navigation and action icons

---
+++ client/src/config/navLinks.ts
@@ -0,0 +1,33 @@
+export const navConfig: Record<string, {
+   href: string;
+   label: string;
+}[]> = {
+   // Default navigation links
+   default:[
+      {
+         href:"/", label:"Home",
+      },
+   ],
+
+
+
+   // Dashboard Links
+   "/dashboard": [
+    {
+      href:"/documents", label:"Documents",
+    },
+    {
+      href:"/cases", label:"Cases",
+    },
+    {
+      href:"/help", label:"Help",
+    },
+  ],
+
+   // Chat Links
+   "/chat": [
+      
+   ]
+
+
+}
